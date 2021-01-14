8c5c46159e46d9356bae5f98bc3c42005c5580aa
Sanzila <sanzila.tuya@rwth-aachen.de>
2021-01-14 20:16:31 +0100

    solver

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const solver = require('./solver');
 const fileUpload = require('express-fileupload');
 var Struct = require('struct');
+var cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || "8080";
 
 app.use(fileUpload());
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
